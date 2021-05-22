b823aa4ff241e9786d586a4f58fa4e34d9d444a5
Philip Borgström <philip.borgstrom@gmail.com>
2021-05-22 12:42:38 +0200

    Add _out as print

---
+++ Assets/Scripts/Control/Shared/In/Print.cs
@@ -0,0 +1,26 @@
+﻿using MoonSharp.Interpreter;
+using System.Text;
+
+namespace Limbie.Control.Shared.In
+{
+    [MoonSharpUserData]
+    public class Print
+    {
+        private readonly StringBuilder output;
+
+        public Print(ref StringBuilder output)
+        {
+            this.output = output;
+        }
+
+        public void WriteLine(string line)
+        {
+            output.AppendLine(line);
+        }
+
+        public void Write(string text)
+        {
+            output.Append(text);
+        }
+    }
+}
