97940fc6714ecdac0f37d6dbcd058dec37190060
Philip Borgström <philip.borgstrom@gmail.com>
2021-05-22 11:10:12 +0200

    Add body

--- Assets/Scripts/Control/Shared/In/Limbs.cs
+++ Assets/Scripts/Control/Shared/In/Limbs.cs
@@ -7,15 +7,15 @@ namespace Limbie.Control.Shared.In
     {
         public Limbs(RobotActor robotActor)
         {
-            facingLimb = new RobotJoint(robotActor.facingLimb);
-            outerFacingLimb = new RobotJoint(robotActor.outerFacingLimb);
-            awayLimb = new RobotJoint(robotActor.awayLimb);
-            outerAwayLimb = new RobotJoint(robotActor.outerAwayLimb);
+            FacingLimb = new RobotJoint(robotActor.facingLimb);
+            OuterFacingLimb = new RobotJoint(robotActor.outerFacingLimb);
+            AwayLimb = new RobotJoint(robotActor.awayLimb);
+            OuterAwayLimb = new RobotJoint(robotActor.outerAwayLimb);
         }
 
-        public RobotJoint facingLimb { get; }
-        public RobotJoint outerFacingLimb { get; }
-        public RobotJoint awayLimb { get; }
-        public RobotJoint outerAwayLimb { get; }
+        public RobotJoint FacingLimb { get; }
+        public RobotJoint OuterFacingLimb { get; }
+        public RobotJoint AwayLimb { get; }
+        public RobotJoint OuterAwayLimb { get; }
     }
 }
